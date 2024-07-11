e2cc1cf126d9a9a8dff153887b3e66a6245edabb
medarcho4 <adammazhidov7@gmail.com>
2024-07-11 22:36:17 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var=("a",1,"b",2,"c",3)
+print(immutable_var)
+mutable_list="BMW","Mercedes", "Audi"
+mutable_list="Ferrari", "Lamborghini", "Maseratti"
+print(mutable_list)
+immutable_var[0]=0 #значения элементов кортежа нельзя изменить, потому что кортеж не поддерживает обращение по элементам
