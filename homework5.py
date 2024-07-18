7f641ecb61809d8cfce782fd2a5c87a621c05531
medarcho4 <adammazhidov7@gmail.com>
2024-07-19 01:28:23 +0300

    homework5.py

--- homework5.py
+++ homework5.py
@@ -1,6 +1,6 @@
 immutable_var=("a",1,"b",2,"c",3)
 print(immutable_var)
-mutable_list="Rolls-Royce", "Ferrari"
-mutable_list="Rolls-Royce", "Ferrari", "Bugatti", "Koenigsegg"
+mutable_list=["Rolls-Royce", "Ferrari"]
+mutable_list.append("Bugatti")
 print(mutable_list)
 immutable_var[0]=0 #значения элементов кортежа нельзя изменить, потому что кортеж не поддерживает обращение по элементам
